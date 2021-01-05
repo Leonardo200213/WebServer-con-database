fd993297b4afeb446acbca153b75b7163b59d0ae
Leonardo200213 <50982851+Leonardo200213@users.noreply.github.com>
2021-01-05 10:20:03 +0100

    aggiunti file esercizio

---
+++ driver/src/main/java/com/mycompany/driver/persona.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.driver;
+
+/**
+ *
+ * @author miste
+ */
+public class persona {
+    String nome;
+    String cognome;
+}
